0aab3494da747c9cfc8419253888d9b2ab349588
Jacquelyn <apps.jhf@gmail.com>
2021-06-17 09:42:42 -0400

    minor title

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'j.h.fede',
       theme: ThemeData(
         primarySwatch: Colors.amber,
       ),
